88283ef7595869e8034817fd82cd3f395160ce0a
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-09-23 16:01:15 +0530

    Update string reversal.cpp

--- string reversal.cpp
+++ string reversal.cpp
@@ -1,3 +1,9 @@
+// EXPERIMENT-15
+// MAITRAIYEE VASHISTHA
+// 24070123057
+// ENTC A3
+
+
 #include <iostream>
 using namespace std;
 
@@ -9,7 +15,13 @@ string reverseString(string s) {
 
 int main() {
     string str;
+    cout<<"Enter the string: ";
     cin >> str;
-    cout << reverseString(str);
+    cout <<"the reversed string for "<<str<<" will be "<<reverseString(str);
     return 0;
 }
+
+/*Output:
+Enter the string: ABCDEFG
+the reversed string for ABCDEFG will be GFEDCBA
+*/
